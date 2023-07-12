29f8a13be10b5e809ef37f8f71390e49ca95fcdb
Kathryn Doering <kathryn.doering@noaa.gov>
2023-07-12 14:16:02 +0000

    tmp: changes for testing, no pat

--- R/input_checks.R
+++ R/input_checks.R
@@ -4,7 +4,7 @@
 #'
 #' Basically check that it is a filename that ends in .yml
 #' @template workflow_name
-check_workflow_name <- function(workflow_name) {
+check_workflow_name <- function(workflow_name){
   stopifnot(is.character(workflow_name))
   stopifnot(length(workflow_name) == 1)
   get_ext <- grep("\\.yml$", workflow_name)
