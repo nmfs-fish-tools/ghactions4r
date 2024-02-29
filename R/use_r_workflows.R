64ae879bed88502b94def02ce78970f151322574
k-doering-NOAA <k-doering-NOAA@users.noreply.github.com>
2024-02-29 17:55:32 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/use_r_workflows.R
+++ R/use_r_workflows.R
@@ -43,9 +43,9 @@ use_r_cmd_check <- function(workflow_name = "call-r-cmd-check.yml",
 #' workflow for calculating code coverage
 #' @template workflow_name
 #' @param use_public_rspm Use posit package manager instead of CRAN to install dependencies? The
-#'  advantage here is that dependencies are precompiled, so install should be much quicker. In 
+#'  advantage here is that dependencies are precompiled, so install should be much quicker. In
 #'  rare situations (like packages with TMB dependencies), using use_public_rspm = FALSE may be
-#'  a better option. Note a setting only needs to be specified in the yml if use_public_rspm is FALSE, so 
+#'  a better option. Note a setting only needs to be specified in the yml if use_public_rspm is FALSE, so
 #'  there will be no setting added if use_public_rspm is TRUE.
 #' @export
 use_calc_coverage <- function(workflow_name = "call-calc-coverage.yml", use_public_rspm = TRUE) {
